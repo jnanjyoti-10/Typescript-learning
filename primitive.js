52f493bf8ae606cac83cb49fc7742e107ce5e2e5
jnanjyotibhunia <jnanjyotibhunia@gmail.com>
2025-05-03 20:41:38 +0530

    add primitive.ts file

---
+++ primitive.js
@@ -0,0 +1,7 @@
+var userName = "Hendy";
+var age = 21;
+var isStudent = true;
+var nothingHere = null;
+var notAssigned = undefined;
+var uniqueId = Symbol("user-id");
+console.log(userName, age, isStudent, uniqueId);
---
+++ primitive.ts
@@ -0,0 +1,8 @@
+let userName: string = "Hendy";
+let age: number = 21;
+let isStudent: boolean = true;
+let nothingHere: null = null;
+let notAssigned: undefined = undefined;
+let uniqueId: symbol = Symbol("user-id");
+
+console.log(userName, age, isStudent, uniqueId);
