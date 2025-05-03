15d5d6eca32f9f64c2b42a6e5fcdbe3f930d65ee
jnanjyotibhunia <jnanjyotibhunia@gmail.com>
2025-05-03 21:13:37 +0530

    add array.ts file

---
+++ Array.js
@@ -0,0 +1,8 @@
+var arr = [1, 2, 3, 4];
+console.log(arr);
+var arr1 = [1, 2, 3, "jnanjyoti"];
+console.log(arr1);
+var number = [1, 2, 3, 4];
+console.log(number);
+var names = ["ram", "modi", "didi"];
+console.log(names);
---
+++ Array.ts
@@ -0,0 +1,11 @@
+let arr=[1,2,3,4];
+console.log(arr);
+
+let arr1=[1,2,3,"jnanjyoti"];
+console.log(arr1);
+
+let number: number[]=[1,2,3,4]
+console.log(number)
+
+let names:string[] =["ram","modi","didi"]
+console.log(names);
