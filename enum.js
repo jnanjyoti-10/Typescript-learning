724c9fdd2a1a7c11494fc925c715bcb16d74b876
jnanjyotibhunia <jnanjyotibhunia@gmail.com>
2025-05-03 21:14:06 +0530

    add enum.ts file

---
+++ enum.js
@@ -0,0 +1,14 @@
+var UserRoles;
+(function (UserRoles) {
+    UserRoles["ADMIN"] = "admin";
+    UserRoles["GUEST"] = "guest";
+    UserRoles["SUPER_ADMIN"] = "super_admin";
+})(UserRoles || (UserRoles = {}));
+console.log(UserRoles.ADMIN);
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["OK"] = 200] = "OK";
+    StatusCode[StatusCode["BadRequest"] = 400] = "BadRequest";
+    StatusCode[StatusCode["NotFound"] = 404] = "NotFound";
+})(StatusCode || (StatusCode = {}));
+console.log(StatusCode.NotFound);
---
+++ enum.ts
@@ -0,0 +1,16 @@
+enum UserRoles{
+    ADMIN="admin",
+    GUEST="guest",
+    SUPER_ADMIN="super_admin"
+}
+
+console.log(UserRoles.ADMIN);
+
+enum StatusCode {
+    OK = 200,
+    BadRequest = 400,
+    NotFound = 404,
+  }
+  
+  console.log(StatusCode.NotFound); 
+  
